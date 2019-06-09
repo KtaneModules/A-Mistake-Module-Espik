0f18eb5d2a45732995f84149c2bc32da96bbe90a
Espik_23 <sbick23@outlook.com>
2019-06-09 18:31:10 -0500

    Version v1.1

--- Assets/MistakeModule.cs
+++ Assets/MistakeModule.cs
@@ -1,9 +1,10 @@
-﻿using System; 
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using KModkit;
+using System.Text.RegularExpressions;
 
 public class MistakeModule : MonoBehaviour {
     public KMAudio Audio;
@@ -19,12 +20,9 @@ public class MistakeModule : MonoBehaviour {
 
     // Solving info
     private int taps = 0;
-    private int time = 0;
-    private String displayTime = "**:**";
 
     private int lastDigit = 0; // Last digit of the serial number
     private int sumOfDigits = 0; // Sum of the digits of the serial number
-
 
     // Ran as bomb loads
     private void Awake() {
@@ -32,8 +30,19 @@ public class MistakeModule : MonoBehaviour {
         Debug.LogFormat("[Mistake #{0}] Uh oh, this isn't supposed to be here.", moduleId);
 
         // Delegation
-        ModuleButton.OnInteract += delegate() {
-            PressModule();
+        ModuleButton.OnInteract += delegate () {
+            ModuleButton.AddInteractionPunch();
+
+            // Logs time pressed
+            Debug.LogFormat("[Mistake #{0}] The module was touched at {1}.", moduleId, Bomb.GetFormattedTime());
+
+            taps++;
+            Taps();
+
+            if (taps == 3) {
+                StartCoroutine(Pause());
+            }
+
             return false;
         };
     }
@@ -43,41 +52,31 @@ public class MistakeModule : MonoBehaviour {
         ModuleButton.GetComponent<MeshRenderer>().material = StageTaps[0];
         lastDigit = Bomb.GetSerialNumberNumbers().Last();
         sumOfDigits = Bomb.GetSerialNumberNumbers().Sum();
-    }
 
-
-    // Module is pressed
-    private void PressModule() {
-        ModuleButton.AddInteractionPunch();
-
-        // Logs time pressed
-        time = GetTime();
-        Debug.LogFormat("[Mistake #{0}] The module was touched at {1}.", moduleId, displayTime);
-
-        taps++;
-        Taps();
-
-        if (taps == 3) {
-            StartCoroutine(Pause());
-        }
+        // Displays solution in log
+        Debug.LogFormat("[Mistake #{0}] First, touch the module anytime.", moduleId);
+        Debug.LogFormat("[Mistake #{0}] Second, touch the module when the last digit of the timer is {1}.", moduleId, lastDigit);
+        Debug.LogFormat("[Mistake #{0}] Third, touch the module when the seconds sections of the timer is {1}.", moduleId, sumOfDigits);
     }
 
     // Module interaction
     private void Taps() {
-        // Current stage
         switch (taps) {
             // First tap
             case 1:
                 StageOne();
                 break;
+
             // Second tap
             case 2:
                 StageTwo();
                 break;
+
             // Third tap
             case 3:
                 StageThree();
                 break;
+
             // Consecutive taps
             default:
                 PostSolve();
@@ -100,7 +99,7 @@ public class MistakeModule : MonoBehaviour {
         Debug.LogFormat("[Mistake #{0}] The module broke a little more!", moduleId);
 
         // If the last digit of the bomb timer is not equal to the last digit of the serial number
-        if (!(time % 10 == lastDigit)) {
+        if (!(((int) Bomb.GetTime()) % 10 == lastDigit)) {
             Debug.LogFormat("[Mistake #{0}] The module was not touched at the right time! It struck!", moduleId);
             Debug.LogFormat("[Mistake #{0}] We told you to touch it when the last digit of the timer is a {1}!", moduleId, lastDigit);
             GetComponent<KMBombModule>().HandleStrike();
@@ -114,7 +113,7 @@ public class MistakeModule : MonoBehaviour {
         Debug.LogFormat("[Mistake #{0}] The module broke even more!", moduleId);
 
         // If the seconds section of the bomb timer are not equal to the sum of the digits of the serial number
-        if (!(time % 60 == sumOfDigits)) {
+        if (!(((int) Bomb.GetTime()) % 60 == sumOfDigits)) {
             Debug.LogFormat("[Mistake #{0}] The module was not touched at the right time! It struck!", moduleId);
             Debug.LogFormat("[Mistake #{0}] We told you to touch it when the sections section of the timer were {1}!", moduleId, sumOfDigits);
             GetComponent<KMBombModule>().HandleStrike();
@@ -128,7 +127,6 @@ public class MistakeModule : MonoBehaviour {
         GetComponent<KMBombModule>().HandleStrike();
     }
 
-
     // Module solving
     private void Solve() {
         Debug.LogFormat("[Mistake #{0}] The module has broken enough! It solved!", moduleId);
@@ -141,15 +139,62 @@ public class MistakeModule : MonoBehaviour {
         Solve();
     }
 
-    // Gets time
-    private int GetTime() {
-        displayTime = Bomb.GetFormattedTime();
-        time = (int) Bomb.GetTime();
-        
-        return time;
+
+    // Twitch Plays - help from Timwi
+#pragma warning disable 414
+    private string TwitchHelpMessage = "!{0} touch [any time] | !{0} touch 1 [when last digit is 1] | !{0} touch 05 [when seconds section is 05]";
+    private bool ZenModeActive;
+#pragma warning restore 414
+
+    // Commands
+    private IEnumerator ProcessTwitchCommand(string command) {
+        Match m;
+
+        // Stage 1 tap
+        if (Regex.IsMatch(command, @"^\s*touch\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)) {
+            yield return null;
+            ModuleButton.OnInteract();
+        }
+
+        // Stage 2 tap
+        else if ((m = Regex.Match(command, @"^\s*touch\s+(\d)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success) {
+            yield return null;
+            int value = int.Parse(m.Groups[1].Value);
+
+            // !cancel-able
+            while (((int) Bomb.GetTime()) % 10 != value)
+                yield return "trycancel";
+
+            ModuleButton.OnInteract();
+        }
+
+        // Stage 3 tap
+        else if ((m = Regex.Match(command, @"^\s*touch\s+(\d\d)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)).Success) {
+            int value = int.Parse(m.Groups[1].Value);
+
+            // If arguement is invalid
+            if (value >= 60) {
+                yield return "sendtochaterror The number of seconds must be 00-59.";
+                yield break;
+            }
+
+            // Elevator music calculation
+            int currentTime = (int) Bomb.GetTime() % 60;
+
+            int interval =
+                ZenModeActive ? (value > currentTime ? value - currentTime : value - currentTime + 60)
+                              : (currentTime > value ? currentTime - value : currentTime - value + 60);
+
+            yield return null;
+
+            if (interval > 10)
+                yield return "waiting music";
+
+            // !cancel-able
+            while (((int) Bomb.GetTime()) % 60 != value)
+                yield return "trycancel";
+
+            ModuleButton.OnInteract();
+        }
     }
-
-
-    // Currently unused
-    private void Update() {}
 }
